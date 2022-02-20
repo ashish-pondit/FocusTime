11cbde29849ba0f9b0f0f2063c3d724f6aa6b09d
Ashish Pondit <48186079+ashish-pondit@users.noreply.github.com>
2022-02-21 02:51:40 +0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 50,
+  },
+});
+
+export default App;
